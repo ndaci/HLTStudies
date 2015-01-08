a7a61b4c63be27c50c8ee835f729f79bc793d70e
ndaci <daci.nadir@gmail.com>
2015-01-08 15:00:03 +0100

    Play with InputTags and Handles.

--- Efficiency/python/efficiencyHLT_cfi.py
+++ Efficiency/python/efficiencyHLT_cfi.py
@@ -1,4 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-eff = cms.EDAnalyzer('Efficiency'
+eff = cms.EDAnalyzer(
+    'Efficiency',
+    hltProcessName   = cms.string("HLT"),
+    genParticleLabel = cms.InputTag("genParticles"),
+    jetCollection    = cms.InputTag("ak5PFJets")
 )
