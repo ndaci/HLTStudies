5c6fa025677f35c503d1b0afce0c5f908012b726
ndaci <daci.nadir@gmail.com>
2015-01-13 16:59:09 +0100

    Remove headers. Start adding branches.

--- Efficiency/plugins/Efficiency.cc
+++ Efficiency/plugins/Efficiency.cc
@@ -1,109 +1,23 @@
-// -*- C++ -*-
-//
-// Package:    HLTStudies/Efficiency
-// Class:      Efficiency
-// 
-/**\class Efficiency Efficiency.cc HLTStudies/Efficiency/plugins/Efficiency.cc
-
- Description: [one line class summary]
-
- Implementation:
-     [Notes on implementation]
-*/
-//
-// Original Author:  ndaci
-//         Created:  Wed, 07 Jan 2015 13:48:51 GMT
-//
-//
-
-
-// C++ lib
-#include <memory>
-
-// CMSSW standard lib
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-
-// CMSSW specific lib
-#include "DataFormats/Candidate/interface/LeafCandidate.h"
-#include "DataFormats/MuonReco/interface/Muon.h"
-#include "DataFormats/MuonReco/interface/MuonFwd.h"
-#include "DataFormats/TrackReco/interface/Track.h"
-#include "DataFormats/TrackReco/interface/TrackFwd.h"
-#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
-#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
-#include "DataFormats/EgammaCandidates/interface/Photon.h"
-#include "DataFormats/EgammaCandidates/interface/PhotonFwd.h"
-#include "DataFormats/METReco/interface/PFMET.h"
-#include "DataFormats/METReco/interface/PFMETCollection.h"
-#include "DataFormats/METReco/interface/GenMET.h"
-#include "DataFormats/METReco/interface/GenMETCollection.h"
-#include "DataFormats/METReco/interface/CaloMET.h"
-#include "DataFormats/METReco/interface/CaloMETCollection.h"
-#include "DataFormats/L1Trigger/interface/L1EtMissParticle.h"
-#include "DataFormats/TauReco/interface/PFTau.h"
-#include "DataFormats/TauReco/interface/PFTauFwd.h"
-#include "DataFormats/JetReco/interface/PFJet.h"
-#include "DataFormats/JetReco/interface/PFJetCollection.h"
-#include "DataFormats/JetReco/interface/CaloJet.h"
-#include "DataFormats/JetReco/interface/CaloJetCollection.h"
-#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
-#include "CommonTools/Utils/interface/StringCutObjectSelector.h"
-#include "DQMServices/Core/interface/DQMStore.h"
-#include "DQMServices/Core/interface/MonitorElement.h"
-#include "HLTrigger/HLTcore/interface/HLTConfigProvider.h"
-
-//
-// class declaration
-//
-
-class Efficiency : public edm::EDAnalyzer {
-   public:
-      explicit Efficiency(const edm::ParameterSet&);
-      ~Efficiency();
-
-      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
-
-
-   private:
-      virtual void beginJob() override;
-      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
-      virtual void endJob() override;
-
-      //virtual void beginRun(edm::Run const&, edm::EventSetup const&) override;
-      //virtual void endRun(edm::Run const&, edm::EventSetup const&) override;
-      //virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
-      //virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
-
-      // ----------member data ---------------------------
-  std::string   _hltProcessName;
-  edm::InputTag _trigResultsLabel;
-  edm::InputTag _genParticleLabel;
-  edm::InputTag _jetCollection;
-
-};
-
-//
-// constants, enums and typedefs
-//
-
-//
-// static data member definitions
-//
+#include "Efficiency.h"
 
 //
 // constructors and destructor
 //
 Efficiency::Efficiency(const edm::ParameterSet& pset) :
-  _hltProcessName(pset.getParameter<std::string>("hltProcessName")),
+
+  _hltProcessName(pset.getParameter<string>("hltProcessName")),
   _trigResultsLabel("TriggerResults", "", _hltProcessName),
-  _genParticleLabel(pset.getParameter<std::string>("genParticleLabel")),
-  _jetCollection(pset.getParameter<edm::InputTag>("jetCollection"))
+  _genParticleLabel(pset.getParameter<string>("genParticleLabel")),
+  _pfjetCollection(pset.getParameter<edm::InputTag>("pfjetCollection")),
+  _pfmetCollection(pset.getParameter<edm::InputTag>("pfmetCollection")),
+  _muCollection(pset.getParameter<edm::InputTag>("muCollection"))
+
 {
-   //now do what ever initialization is needed
+
+  //now do what ever initialization is needed
+  edm::Service<TFileService> fs ;
+  _tree = fs->make <TTree>("HLTStudies","Efficiency");
+
 }
 
 
@@ -125,12 +39,44 @@ void
 Efficiency::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
 
-  //edm::Handle<edm::TriggerResults> trigResults;
 
   //edm::Handle<reco::GenParticleCollection> genPart;
 
+  // Get collections
+  edm::Handle<edm::TriggerResults> H_trig;
+  iEvent.getByLabel(_trigResultsLabel, H_trig);
+
   edm::Handle<reco::PFJetCollection> H_pfjets;
-  iEvent.getByLabel(_jetCollection , H_pfjets);
+  iEvent.getByLabel(_pfjetCollection , H_pfjets);
+
+  edm::Handle<reco::PFMETCollection> H_pfmet;
+  iEvent.getByLabel(_pfmetCollection , H_pfmet);
+
+  edm::Handle<reco::MuonCollection> H_mu;
+  iEvent.getByLabel(_muCollection , H_mu);
+
+  // Check validity
+  if(!H_trig.isValid()) {
+    if(verbose>0) cout << "Missing collection : " << _trigResultsLabel << " ... skip entry !" << endl;
+    return;
+  }
+
+  if(!H_pfjets.isValid()) {
+    if(verbose>0) cout << "Missing collection : " << _pfjetCollection << " ... skip entry !" << endl;
+    return;
+  }
+
+  if(!H_pfmet.isValid()) {
+    if(verbose>0) cout << "Missing collection : " << _pfmetCollection << " ... skip entry !" << endl;
+    return;
+  }
+
+  if(!H_mu.isValid()) {
+    if(verbose>0) cout << "Missing collection : " << _muCollection << " ... skip entry !" << endl;
+    return;
+  }
+
+  
 
 }
 
