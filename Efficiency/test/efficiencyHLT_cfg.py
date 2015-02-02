f86a60329cf2a8a67529cbcb025cd957ceb1355e
ndaci <daci.nadir@gmail.com>
2015-02-02 23:31:34 +0100

    Processing changes.

--- Efficiency/test/efficiencyHLT_cfg.py
+++ Efficiency/test/efficiencyHLT_cfg.py
@@ -4,7 +4,7 @@ process = cms.Process("EFF")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(5) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
@@ -12,6 +12,10 @@ process.source = cms.Source("PoolSource",
         'file:hltSimMonojet_Phys14_14e33_100_1_SOi.root'
     )
 )
+
+#from list_MonojetM1AV_r731_i731HLT1_V13_7e33_V2_cff import *
+from list_MonojetM1AV_r731_i731HLT1_V13_14e33_cff import *
+process.source = source
 
 process.load("HLTStudies.Efficiency.efficiencyHLT_cfi")
 process.eff.namePaths = cms.vstring("")
