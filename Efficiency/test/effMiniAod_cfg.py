60e69c61ac97df3bc9eb33f430852e1e94c7483c
ndaci <daci.nadir@gmail.com>
2015-06-08 12:30:35 +0200

    Change inputs and maxEvents for tests.

--- Efficiency/test/effMiniAod_cfg.py
+++ Efficiency/test/effMiniAod_cfg.py
@@ -5,7 +5,7 @@ process = cms.Process("EFFMINIAOD")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(5) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 tag=""
 for i in range(0,len(sys.argv)):
@@ -17,7 +17,8 @@ process.source = cms.Source("PoolSource",
         #'file:hlt_100_1_knd.root'
         #'file:hltSimMonojet_Phys14_14e33_100_1_SOi.root'
         #'file:/user/ndaci/Data/DarkMonojet/TestHCAL/hltSim10000_Method3.root'
-        'file:/user/ndaci/Data/MiniAOD/miniAOD-prod_PAT.root'
+        #'file:/user/ndaci/Data/MiniAOD/miniAOD-prod_PAT.root'
+        'file:/user/ndaci/Data/MiniAOD/store/relval/CMSSW_7_4_4/RelValADDMonoJet_d3MD3_13/MINIAODSIM/MCRUN2_74_V9_38Tbis-v1/00000/3A4505E8-2F09-E511-8459-003048FFD732.root'
     )
 )
 
